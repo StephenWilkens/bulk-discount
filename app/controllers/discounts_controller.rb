1196b95365f9e8d6ef6df16cde93ea3c8434076b
Stephen Wilkens <stephenwilkens@gmail.com>
2022-06-11 19:43:25 -0600

    Add partial form, new view/spec, new and create actions, new and create routes, and index test for link

--- app/controllers/discounts_controller.rb
+++ app/controllers/discounts_controller.rb
@@ -6,4 +6,25 @@ class DiscountsController < ApplicationController
   def show
     @discount = Discount.find(params[:discount_id])
   end
+
+  def new
+    @merchant = Merchant.find(params[:merchant_id])
+    @discount = Discount.new
+  end
+
+  def create
+    @merchant = Merchant.find(params[:merchant_id])
+    discount = Discount.new(discount_params)
+    if discount.save
+      redirect_to "/merchants/#{@merchant.id}/discounts"
+    else
+      flash[:alert] = 'Unable to Create Discount'
+      redirect_to "/merchants/#{@merchant.id}/discounts/new"
+    end
+  end
+
+  private
+  def discount_params
+    params.permit(:name, :percentage_discount, :quantity_threshold, :merchant_id)
+  end
 end
